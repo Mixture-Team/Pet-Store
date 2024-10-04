2a311930d9ba8d5371aad530d26cfea9b87671f1
hoangnguyen <vanhoang1231234@gmail.com>
2024-10-04 22:28:42 +0700

    Init project

---
+++ src/main/java/mixture/hutech/petstore/PetStoreApplication.java
@@ -0,0 +1,13 @@
+package mixture.hutech.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetStoreApplication.class, args);
+    }
+
+}
