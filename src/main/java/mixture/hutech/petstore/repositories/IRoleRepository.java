e0fcad68f2d44ddc3502e17813d2ebacda6c1999
hoangnguyen <vanhoang1231234@gmail.com>
2024-10-17 23:56:59 +0700

    feat: authentication + oauth2

---
+++ src/main/java/mixture/hutech/petstore/repositories/IRoleRepository.java
@@ -0,0 +1,11 @@
+package mixture.hutech.petstore.repositories;
+
+
+import hutech.mixture.petstore.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role,Long> {
+    Role findRoleById(Long id);
+}
