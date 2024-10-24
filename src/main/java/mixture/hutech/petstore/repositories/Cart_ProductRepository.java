87c36aae3a80359c301e9ae71fae824a721dddfa
hoangnguyen <vanhoang1231234@gmail.com>
2024-10-24 20:32:31 +0700

    feat: cart

---
+++ src/main/java/mixture/hutech/petstore/repositories/Cart_ProductRepository.java
@@ -0,0 +1,12 @@
+package mixture.hutech.petstore.repositories;
+
+
+import hutech.mixture.petstore.models.Cart_Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface Cart_ProductRepository extends JpaRepository<Cart_Product, Long> {
+    List<Cart_Product> findByCartId(Long cartId);
+}
---
+++ src/main/java/mixture/hutech/petstore/services/CartService.java
@@ -0,0 +1,182 @@
+package mixture.hutech.petstore.services;
+
+import hutech.mixture.petstore.models.*;
+import hutech.mixture.petstore.repositories.CartRepository;
+import hutech.mixture.petstore.repositories.Cart_ProductRepository;
+import hutech.mixture.petstore.repositories.DistrictRepository;
+import hutech.mixture.petstore.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CartService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private DistrictRepository districtRepository;
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private Cart_ProductRepository cartProductRepository;
+
+    @Transactional
+    public Cart createOrder(String customerName, String shippingAddress, String phoneNumber, String notes, String paymentMethod, List<CartItem> cartItems, double totalPrice, Long districtId, double totalShippingPrice) {
+
+        // Lấy người dùng hiện tại
+        Long currentUserId = userService.getCurrentUserId();
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Cart order = new Cart();
+        order.setCustomerName(customerName);
+        order.setAddress(shippingAddress);
+        order.setPhone(phoneNumber);
+        order.setNotes(notes);
+        order.setTotalPrice(totalPrice);
+        order.setTotalshippingprice(totalShippingPrice);
+        order.setPaymentMethods(paymentMethod);
+
+        order.setDateBegin(LocalDateTime.now());
+        order.setDateEnd(LocalDateTime.now());
+        order.setOrderStatus("Đang xử lý");
+        order.setTradingCode("0");
+
+        District district = districtRepository.findById(districtId).orElseThrow(() -> new RuntimeException("District not found"));
+        order.setDistrict(district);
+        order.setUser(currentUser);
+
+        // Lưu đơn hàng
+        order = cartRepository.save(order);
+
+        // Lưu chi tiết đơn hàng
+        for (CartItem item : cartItems) {
+            Cart_Product detail = new Cart_Product();
+            detail.setQuantity(item.getQuantity());
+            detail.setTotal_price(item.getTotal_price());
+            detail.setCart(order);
+            detail.setProduct(item.getProduct());
+            detail.setServiceDetail(item.getServiceDetail());
+            cartProductRepository.save(detail);
+        }
+        List<Cart_Product> cartProducts = cartProductRepository.findByCartId(order.getId());
+        order.setCartProducts(cartProducts);
+        return order;
+    }
+
+    public Cart saveOrder(Cart order) {
+        return cartRepository.save(order);
+    }
+
+    public List<Cart> getOrdersForLoggedInUser() {
+        Long currentUserId = userService.getCurrentUserId();
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return cartRepository.findByUser(currentUser);
+    }
+    public Optional<Cart> getOrderById(Long orderId) {
+        return cartRepository.findById(orderId);
+    }
+    public Page<Cart> getAllCartForAdmin(Pageable pageable){
+        return cartRepository.findAll(pageable);
+    }
+
+    public Cart getCartById(Long id){
+        return cartRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Không tìm thấy đơn hàng có id " + id)
+        );
+    }
+
+    @Transactional
+    public Cart updateCart(Cart cart){
+        Cart existingCart = cartRepository.findById(cart.getId()).orElseThrow(
+                () -> new RuntimeException("Không tìm thấy đơn hàng có id " + cart.getId())
+        );
+
+        District district = districtRepository.findById(cart.getDistrict().getId()).orElseThrow(
+                () -> new RuntimeException("Không tìm thấy huyện có id " + cart.getDistrict().getId())
+        );
+
+        existingCart.setCustomerName(cart.getCustomerName());
+        existingCart.setPhone(cart.getPhone());
+        existingCart.setNotes(cart.getNotes());
+        existingCart.setDateBegin(cart.getDateBegin());
+        existingCart.setAddress(cart.getAddress());
+        existingCart.setPaymentMethods(cart.getPaymentMethods());
+        existingCart.setTotalPrice(cart.getTotalPrice());
+        existingCart.setTotalshippingprice(cart.getTotalshippingprice());
+        existingCart.setTradingCode(cart.getTradingCode());
+        existingCart.setUser(cart.getUser());
+        existingCart.setDistrict(district);
+
+        if(cart.getOrderStatus().equals("Giao hàng thành công") || cart.getOrderStatus().equals("Huỷ")){
+            existingCart.setDateEnd(LocalDateTime.now());
+        }
+
+        existingCart.setOrderStatus(cart.getOrderStatus());
+        cartRepository.save(existingCart);
+        return existingCart;
+    }
+
+    public Page<Cart> searchCartsByPhone(String phone, Pageable pageable){
+        return cartRepository.findByPhoneContaining(phone,pageable);
+    }
+
+    @Transactional
+    public Cart createOrder1(String customerName, String shippingAddress, String phoneNumber, String notes, String paymentMethod, List<CartItem> cartItems, double totalPrice, Long districtId, double totalShippingPrice, String tradingCode) {
+
+        // Lấy người dùng hiện tại
+        Long currentUserId = userService.getCurrentUserId();
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Cart order = new Cart();
+        order.setCustomerName(customerName);
+        order.setAddress(shippingAddress);
+        order.setPhone(phoneNumber);
+        order.setNotes(notes);
+        order.setTotalPrice(totalPrice);
+        order.setTotalshippingprice(totalShippingPrice);
+        order.setPaymentMethods(paymentMethod);
+
+        order.setDateBegin(LocalDateTime.now());
+        order.setDateEnd(LocalDateTime.now());
+        order.setOrderStatus("Đang xử lý");
+        order.setTradingCode(tradingCode);
+
+        District district = districtRepository.findById(districtId).orElseThrow(() -> new RuntimeException("District not found"));
+        order.setDistrict(district);
+        order.setUser(currentUser);
+
+        // Lưu đơn hàng
+        order = cartRepository.save(order);
+
+        // Lưu chi tiết đơn hàng
+        for (CartItem item : cartItems) {
+            Cart_Product detail = new Cart_Product();
+            detail.setQuantity(item.getQuantity());
+            detail.setTotal_price(item.getTotal_price());
+            detail.setCart(order);
+            detail.setProduct(item.getProduct());
+            detail.setServiceDetail(item.getServiceDetail());
+            cartProductRepository.save(detail);
+        }
+
+        return order;
+    }
+
+    public Cart findByTradingCode(String tradingCode){
+        return cartRepository.findByTradingCode(tradingCode);
+    }
+
+
+}
